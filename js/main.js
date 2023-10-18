2bec2c2c6cc89b4298c5829e4b00ad70139eea50
Nikita Kostevich <nikitakostevich@gmail.com>
2023-10-18 21:13:56 +0300

    Добавляет функции для коментариев

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const NAMES = [
+  'БРАНОСТАЛЬФ',
+  'ФРЕДИД',
+  'ТУБРАНО',
+  'АКМО',
+  'ЭРГАР',
+];
+
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо.Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают.Как можно было поймать такой неудачный момент ? !',
+];
+
+const DESCRIPTION = [
+  'Изображение красного ретро-автомобиля',
+  'Вид на горы',
+  'Заход солнца на берегу океана',
+  'Звездное небо с большой медведицей',
+  'Звездное небо с созведием стрельца',
+];
+
+function getRandomInteger(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const generateIdComment = getRandomInteger(1, 25);
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+const generateIdForUser = getRandomInteger(1, 25);
+const generatePhoto = getRandomInteger(1, 25);
+
+const createCommentsArray = () => ({
+  commentsId: generateIdComment,
+  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  message: MESSAGE[Math.round(Math.random() * (MESSAGE.length - 1))],
+  name: getRandomArrayElement(NAMES),
+});
+
+const createUserPhoto = () => ({
+  userId: generateIdForUser,
+  urlPhoto: `photos/${generatePhoto}.jpg`,
+  description: getRandomArrayElement(DESCRIPTION),
+  likes: getRandomInteger(15, 200),
+  comments: Array.from({ length: getRandomInteger(0, 30) }, createCommentsArray),
+});
+
+const generatedPosts = Array.from({ length: 30 }, createUserPhoto);
+
+console.log(generatedPosts);
+
